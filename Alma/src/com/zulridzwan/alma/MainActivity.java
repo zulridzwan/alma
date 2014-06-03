77d9e36b8e08259e5ed5fed1c38e0ba4c994a35d
zulridzwan <zulridzwan@gmail.com>
2014-06-03 16:55:06 +0800

    First Activity

--- Alma/src/com/zulridzwan/alma/MainActivity.java
+++ Alma/src/com/zulridzwan/alma/MainActivity.java
@@ -2,7 +2,12 @@ package com.zulridzwan.alma;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button1 = (Button)findViewById(R.id.button1);
+		EditText etext1 = (EditText)findViewById(R.id.editText1);
+		final String txt = etext1.getText().toString();
+		
+		button1.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+				intent.putExtra("URL", "http://www.vogella.com");
+				intent.putExtra("Text",txt);
+				startActivity(intent);
+			}
+			
+		});
+		
 	}
 
 	@Override
